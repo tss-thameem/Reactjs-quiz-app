20ec6c9576d5a381e5bb36e18dfc1a84645471c4
Thameem Ansari <thameem@tecnovators.in>
2021-09-03 18:57:15 +0530

    My First Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/style.css';
+import quizService from './quizService/index.js'; //quizService name at use the 14th line
+import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
+
+class QuizApp extends Component { 
+  state={
+       questionBank: [], //questionBox name any name use to run But I changed to questionBank.
+       score:0,
+       responses:0
+      }
+  getQuestions =() => { 
+  quizService().then(question=>{
+    this.setState({
+         questionBank:question
+    })
+     })
+      }
+  computeAnswer = (answer,correctAnswer) => {
+     if(answer === correctAnswer) {
+       this.setState({
+         score: this.state.score +1
+       })
+      }
+       this.setState({
+         responses: this.state.responses<5 ? this.state.responses+1 : 5
+       })
+     
+}
+  playAgain =()=>{
+   this.getQuestions();
+    this.setState({
+      score:0,
+      responses:0
+    })
+  }
+    componentDidMount(){
+      this.getQuestions();
+      }
+  render() {
+    return (
+      <div className="container">
+        <div className="title">Welcome to Quiz!<br></br><span style={{ fontSize:'49px', color: 'yellow' }}>Choose the Best Answer</span></div>
+        {this.state.questionBank.length > 0 && this.state.responses<5 &&
+        this.state.questionBank.map(({ question, answers, correct, questionId})=>
+        <QuestionBox question={question} 
+                     options={answers} 
+                     selected={answer => this.computeAnswer(answer, correct)}
+                     key={questionId}/>)}
+         {this.state.responses ==5 ? <Result score={this.state.score} playAgain={this.playAgain}/> :null}            
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QuizApp />,
   document.getElementById('root')
 );
 
